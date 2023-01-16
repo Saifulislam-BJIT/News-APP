9d42bfe8d60abbadcf71c43cf7b3e1326501ee44
Saiful Islam <saiful.islam@bjitacademy.com>
2023-01-16 22:10:14 +0600

    add api

---
+++ app/src/main/java/com/saiful/newsapp/network/NewsApiService.kt
@@ -0,0 +1,29 @@
+package com.saiful.newsapp.network
+
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import retrofit2.http.GET
+
+private const val BASE_URL = "https://newsapi.org/v2/"
+private const val TOKEN = "9d68e0d1f113454cb2e1e6ad604ac096"
+private const val BASE_URL = "https://android-kotlin-fun-mars-server.appspot.com"
+
+private val moshi = Moshi.Builder()
+    .add(KotlinJsonAdapterFactory())
+    .build()
+
+private val retrofit = Retrofit.Builder()
+    .addConverterFactory(MoshiConverterFactory.create(moshi))
+    .baseUrl(BASE_URL)
+    .build()
+
+object MarsApi {
+    val retrofitService: NewsApiService by lazy { retrofit.create(NewsApiService::class.java) }
+}
+
+interface NewsApiService {
+    @GET("photos")
+    suspend fun getPhotos() : String
+}
