b96b70e005825ead83a1782e0447eff67438e68c
Saiful Islam <saiful.islam@bjitacademy.com>
2023-01-26 14:49:40 +0600

    add second api

--- app/src/main/java/com/saiful/newsapp/constant/Constant.kt
+++ app/src/main/java/com/saiful/newsapp/constant/Constant.kt
@@ -8,7 +8,8 @@ import com.saiful.newsapp.database.NewsArticle
 class Constant {
     companion object {
         const val BASE_URL = "https://newsapi.org/v2/"
-        const val TOKEN = "b9aec1e5c45b4c27b89627110fe05e45"
+//        const val TOKEN = "b9aec1e5c45b4c27b89627110fe05e45"
+        const val TOKEN = "9d68e0d1f113454cb2e1e6ad604ac096"
 
         var category: String = Category.BUSINESS
         var newsArticle: NewsArticle? = null
